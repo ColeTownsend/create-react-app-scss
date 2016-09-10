90c293a120bfa3506cf51255515ca471ce301fae
Cole Townsend <cole@coletownsend.com>
2016-09-10 11:36:14 -0400

    Add Scss and HMR to react create app - add version number

--- template/src/index.js
+++ template/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
+
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 );
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
